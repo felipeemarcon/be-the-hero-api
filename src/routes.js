549074903242ba7948a41324c87f459141f65da0
Felipe Marcon <felipeemarcon@gmail.com>
2020-03-25 23:35:43 -0300

    feat: create endpoints for incidents

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,14 @@ const routes = express.Router();
 
 // Controllers
 const OngController = require("./controllers/OngController");
+const IncidentController = require("./controllers/IncidentController");
 
 // Ongs endpoints
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
+
+// Ongs Incidents
+routes.get("/incidents", IncidentController.index);
+routes.post("/incidents", IncidentController.create);
 
 module.exports = routes;
